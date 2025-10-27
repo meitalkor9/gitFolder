0e106eb0fdd7ec1d54adb9824bc6d66964da7867
meitalkor9 <meitalkor9@gmail.com>
2025-10-27 11:22:59 +0200

    print-NCommonWords

--- frequencyWords.py
+++ frequencyWords.py
@@ -10,3 +10,16 @@ for word in words:
     wordsDict[word]=wordsDict[word]+1
 ##רשימה ממוינת לפי הערך
 sortedList=sorted(wordsDict.items(), key=lambda item: item[1], reverse=True)
+##הדפסת המילים הנפוצות
+import sys
+try:
+    N = int(sys.argv[1])
+    for i in range(1,N+1):
+        print(i,"- word",sortedList[i-1][0]," ",sortedList[i-1][1]," times")
+        
+except ValueError:
+    print("Error: הפרמטר שהוזן אינו מספר")
+    sys.exit(1)
+except IndexError:
+    print("Error: לא קיימים כל כך הרבה מילים בקובץ")
+    sys.exit(1)
