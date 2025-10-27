011a8ff30608f7ae6bcca1fd170559b90697ed8f
meitalkor9 <meitalkor9@gmail.com>
2025-10-27 09:59:59 +0200

    wordToKey

--- frequencyWords.py
+++ frequencyWords.py
@@ -1,3 +1,7 @@
 #קריאת הקובץ והשמה ברשימה
 with open('fileFor_Frequency.txt', 'r') as file:
     words = file.read().split()
+#כל מילה בקובץ הופכת למפתח במילון
+wordsDict={}
+for word in words:
+    wordsDict[word]=0
